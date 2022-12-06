6865a9a9144186e751b2baed4e98e1d3d4476fa0
Bobby Galli <bobbyg603@gmail.com>
2022-12-05 19:16:49 -0500

    chore: ran update from linux

--- tools/linux/update.sh
+++ tools/linux/update.sh
@@ -1,7 +1,7 @@
 # Variables
 CRASHPAD_DIR="/home/bobby/Desktop/github/crashpad"
 ANDROID_ABIS=( "arm64-v8a" "armeabi-v7a" "x86" "x86_64" )
-PROJECT_DIR="/home/bobby/Desktop/github/my-android-crasher"
+PROJECT_DIR="/home/bobby/Desktop//my-android-crasher"
 
 # Start in the Crashpad dir
 cd $CRASHPAD_DIR
