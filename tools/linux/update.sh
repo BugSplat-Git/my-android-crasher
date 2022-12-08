614f82acd6fa0a2c5aa10bb2ded2bc98be734d43
Bobby Galli <bobbyg603@gmail.com>
2022-12-08 09:16:32 -0500

    chore: remove object files

--- tools/linux/update.sh
+++ tools/linux/update.sh
@@ -1,7 +1,7 @@
 # Variables
 CRASHPAD_DIR="/home/bobby/Desktop/github/crashpad"
 ANDROID_ABIS=( "arm64-v8a" "armeabi-v7a" "x86" "x86_64" )
-PROJECT_DIR="/home/bobby/Desktop//my-android-crasher"
+PROJECT_DIR="/home/bobby/Desktop/github/my-android-crasher"
 
 # Start in the Crashpad dir
 cd $CRASHPAD_DIR
@@ -9,6 +9,9 @@ cd $CRASHPAD_DIR
 # Update Crashpad
 git pull -r
 gclient sync
+
+# Copy .h Includes
+rsync -avh --include='*/' --include='*.h' --exclude='*' --prune-empty-dirs ./ $PROJECT_DIR/app/src/main/cpp/crashpad/include
 
 for ANDROID_ABI in "${ANDROID_ABIS[@]}"
 do
@@ -18,15 +21,19 @@ do
     # Build Crashpad
     ninja -C $CRASHPAD_OUT
 
-    # Copy .h Includess
-    rsync -avh --include='*/' --include='*.h' --exclude='*' --prune-empty-dirs ./ $PROJECT_DIR/app/src/main/cpp/crashpad/include
+    # Copy chrome_buildflags.h
     mkdir -p $PROJECT_DIR/app/src/main/cpp/crashpad/include/build
     cp $CRASHPAD_OUT/gen/build/chromeos_buildflags.h $PROJECT_DIR/app/src/main/cpp/crashpad/include/build
 
     # Copy Libraries
-    cp -R $CRASHPAD_OUT/obj/client $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI
-    cp -R $CRASHPAD_OUT/obj/util $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI
-    cp -R $CRASHPAD_OUT/obj/third_party/mini_chromium/mini_chromium/base $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI
+    mkdir -p $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI
+    mkdir -p $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/base
+    mkdir -p $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/client
+    mkdir -p $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/util
+    cp $CRASHPAD_OUT/obj/client/libclient.a $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/client
+    cp $CRASHPAD_OUT/obj/client/libcommon.a $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/client
+    cp $CRASHPAD_OUT/obj/util/libutil.a $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/util
+    cp $CRASHPAD_OUT/obj/third_party/mini_chromium/mini_chromium/base/libbase.a $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI/base
 
     # Copy Handler
     cp $CRASHPAD_OUT/libcrashpad_handler.so $PROJECT_DIR/app/src/main/cpp/crashpad/lib/$ANDROID_ABI
